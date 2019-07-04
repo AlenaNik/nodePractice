b1f8948eeb851f2b68acde4d11d7e1742b628e10
AlenaNik <alena.nik.digital@gmail.com>
2019-07-04 13:00:02 +0200

    before thrird exircise

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input : process.stdout,
-    output : process.stdout
-});
-let num1 = Math.floor((Math.random() * 10) + 1);
-let num2 = Math.floor((Math.random() * 10) + 1);
-let answer = num1 + num2;
 
-rl.question(`What is ${ num1 } + ${ num2 }? \n`,
-    (userInput) => {
-    if (userInput.trim() == answer) {
 
-    } else {
-        rl.setPrompt('Incorect response\n');
-        rl.prompt();
-        rl.on('line',(userInput) => {
-            if(userInput.trim() == answer)
-                rl.close();
-            else {
-                rl.setPrompt(`Your anwer of ${ userInput } is incorrect \n`)
-                rl.prompt();
-            }
-        })
-    }
-});
-rl.on('close',() => {
-    console.log('Nice one');
-});
+
+// Exersice 2
+
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input : process.stdout,
+//     output : process.stdout
+// });
+// let num1 = Math.floor((Math.random() * 10) + 1);
+// let num2 = Math.floor((Math.random() * 10) + 1);
+// let answer = num1 + num2;
+//
+// rl.question(`What is ${ num1 } + ${ num2 }? \n`,
+//     (userInput) => {
+//     if (userInput.trim() == answer) {
+//
+//     } else {
+//         rl.setPrompt('Incorect response\n');
+//         rl.prompt();
+//         rl.on('line',(userInput) => {
+//             if(userInput.trim() == answer)
+//                 rl.close();
+//             else {
+//                 rl.setPrompt(`Your anwer of ${ userInput } is incorrect \n`)
+//                 rl.prompt();
+//             }
+//         })
+//     }
+// });
+// rl.on('close',() => {
+//     console.log('Nice one');
+// });
+
+// Exersice 1
 // const EventEmitter = require('events');
 // const eventEmitter = new EventEmitter();
 //
